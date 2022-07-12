a1955a80b1f5b4eeb8c0c9b55aec30c2e5f97c33
Shay <shaybush93@gmail.com>
2022-07-12 18:50:11 +0300

    all fix about and trivia not done

--- script/filter_script.js
+++ script/filter_script.js
@@ -7,6 +7,7 @@
             paris = document.querySelector("#paris");
             amsterdam = document.querySelector("#amsterdam");
             arr = [rhodes, rome, budapest, paris, amsterdam];
+            cities = ['rhodes', 'rome', 'budapest', 'paris', 'amsterdam'];
             arr.forEach((e) => {e.classList.add("active");})
         }
         const change_Country = () => {
@@ -16,71 +17,23 @@
             // לא להתייחס
             if(select_country == "all")
             {
-                document.querySelector("#id_span").innerHTML = "Each Contry";
+                document.querySelector("#id_span").innerHTML = "";
             }
             else{
-                document.querySelector("#id_span").innerHTML = select_country;
+                document.querySelector("#id_span").innerHTML = ` in ${select_country}`;
             }
             // עד לפה
-
-            // אני רוצה במקום לבדוק אחד אחד 
-            // להשתמש בערך של select 
-            // ושהוא יזהה את זה כאובייקט שלי למעלה
-            if (select_country == 'rhodes') {
-                arr.forEach((e, index) => {
-                    if (index == 0) {
-                        // e.classList.add("active");
+            cities.forEach((e, index) =>{
+                if(e == select_country){
+                    arr[index].classList.add("active");
+                }
+                else if(select_country == "all"){
+                    arr.forEach((e) => {
                         e.classList.add("active");
-                    }
-                    else {
-                        e.classList.remove("active");
-                    }
-                })
-            }
-            else if (select_country == 'rome') {
-                arr.forEach((e, index) => {
-                    if (index == 1) {
-                        e.classList.add("active");
-                    }
-                    else {
-                        e.classList.remove("active");
-                    }
-                })
-                // arr[1].classList.add("active");
-            }
-            else if (select_country == 'budapest') {
-                arr.forEach((e, index) => {
-                    if (index == 2) {
-                        e.classList.add("active");
-                    }
-                    else {
-                        e.classList.remove("active");
-                    }
-                })
-            }
-            else if (select_country == 'paris') {
-                arr.forEach((e, index) => {
-                    if (index == 3) {
-                        e.classList.add("active");
-                    }
-                    else {
-                        e.classList.remove("active");
-                    }
-                })
-            }
-            else if (select_country == 'amsterdam') {
-                arr.forEach((e, index) => {
-                    if (index == 4) {
-                        e.classList.add("active");
-                    }
-                    else {
-                        e.classList.remove("active");
-                    }
-                })
-            }
-            else {
-                arr.forEach((e, index) => {
-                    e.classList.add("active");
-                })
-            }
+                    })
+                }
+                else{
+                    arr[index].classList.remove("active");
+                }
+            })
         }
